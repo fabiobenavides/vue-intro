08e7e815218b61d08a411e38644a9284df849c75
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2022-12-20 15:38:51 -0600

    Router and Navigation changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <router-link :to="{name:'Home'}">
+            <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name:'Build'}">
+            Build-a-Bot
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  name: 'app',
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+</style>
+
+<style scoped>
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
